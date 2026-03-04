6c19034b624b2812ab7eb22dbb86a2bd51d6b5fd
Nsimba Afonso <nsimba2025nwayami@gmail.com>
2026-03-04 01:29:43 +0100

    Alteração pontual

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-//const API_URL = "https://api.nwayami.com/api/";
-const API_URL = "/api/";
+const API_URL = "https://api.nwayami.com/api/";
+//const API_URL = "/api/";
 
 export const api = axios.create({
   baseURL: API_URL,
